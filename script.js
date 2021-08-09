c828f25fa115a85eaee6454ccedf6a082f31b5ae
Jordan <jordansoh14@gmail.com>
2021-08-09 18:38:43 -0500

    update

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-let timerEl = document.querySelector(.timer)
-let questionBtn = document.querySelector(.question)
-let box = document.querySelector(.container)
+let startButton = document.querySelector(".start-btn");
+let questionContainerEl = document.querySelector("#question-container");
+let questionEl = document.getElementById(".question");
+let answerButtonEl = document.getElementById("#answer-btns");
+startButton,addEventListener("click", startGame);
 
-function startQuiz () {
+let shuffledQuestions, currentQuestionIndex
 
-    timerCount = 75;
-    startTimer()
+
+function startGame() {
+console.log("Started");
+startButton.classList.add('hide');
+questionContainerEl.classList.remove('hide');
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+nextQuestion()
+
 }
 
-function startTimer () {
-    timer = setInterval(function(){
-        timerCount--;
-        timerEl.innerHTML = timerCount;
-    }, 1000);
+function nextQuestion() {
+showQuestion(shuffledQuestions[currentQuestionIndex])
+
 }
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    
+}
+
+function selectAnswer() {
+
+
+}
+
+function setTime() {
+
+
+}
+
+const questions = [
+    {
+        question: 'What is 2+2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '72', correct: false }
+        ]
+    }
+]
