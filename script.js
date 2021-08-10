91ac95c190f2ee9b61cf0b056501c8693aa0203b
Jordan <jordansoh14@gmail.com>
2021-08-10 08:43:14 -0500

    update

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
 let startButton = document.querySelector(".start-btn");
+let restartEl = document.querySelector(".restart-btn");
 let questionContainerEl = document.querySelector("#question-container");
 let questionEl = document.getElementById(".question");
 let answerButtonEl = document.getElementById("#answer-btns");
+let timerEl = document.querySelector("#timer-count")
 startButton,addEventListener("click", startGame);
 
-let shuffledQuestions, currentQuestionIndex
+let timer;
+
+let questionsOptions = [
+    {
+        question: "what is 2+2?",
+        answers: {4, 18, 76, 9}, 
+         key: "0"
+    }
+    
+
+    // {question: "What color is the sky?",
+    // answer: "Red" "yellow" "blue" "47",
+    // key: 2},
+
+    // {question: "A ____ is a true/false statement.",
+    // answer: "String" "Number" "Boolean" "33",
+    // key: 2},
+
+    // {question: "How many states are in the in United States?",
+    // answer: "30" "40" "50" "60",
+    // key: 2},
+]
+
 
 
 function startGame() {
 console.log("Started");
 startButton.classList.add('hide');
 questionContainerEl.classList.remove('hide');
-shuffledQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
+timerCount = 75
 nextQuestion()
-
+setTime()
 }
+
+
 
 function nextQuestion() {
-showQuestion(shuffledQuestions[currentQuestionIndex])
-
+    
+    
 }
 
-function showQuestion(question) {
-    questionEl.innerText = question.question
+function showQuestion() {
+    
     
 }
 
 function selectAnswer() {
-
-
+    
+    
 }
 
 function setTime() {
+    timer = setInterval(function(){
+        timerCount--;
+        timerEl.textContent = timerCount;
 
-
+        if(timerCount === 0){
+            clearInterval(timerCount);
+        }
+    }, 10);
+    
 }
 
-const questions = [
-    {
-        question: 'What is 2+2?',
-        answers: [
-            { text: '4', correct: true },
-            { text: '72', correct: false }
-        ]
-    }
-]
+
